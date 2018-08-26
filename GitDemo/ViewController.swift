5b4f5c9fac6ad0eff16f424cc0b15ac24f9bdf3e
PRABHU TANNIRU <PRABHU>
2018-08-26 11:20:50 +0530

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by PRABHU TANNIRU on 26/08/18.
+//  Copyright © 2018 siyasofticon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
